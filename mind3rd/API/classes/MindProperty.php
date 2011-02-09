ba2a53110a8881afdc80e8aa2d8964c3fb6fa603
Felipe Nascimento de Moura <felipenmoura@gmail.com>
2011-02-08 23:50:52 -0200

    Added annotations to MindProperty class

--- mind3rd/API/classes/MindProperty.php
+++ mind3rd/API/classes/MindProperty.php
@@ -1,6 +1,6 @@
 <?php
 	/**
-	 * Description of MindProperty
+	 * Represents the property of an entity(MindEntity::properties)
 	 *
 	 * @author felipe
 	 */
@@ -10,17 +10,29 @@
 		public $name= false;
 		public $type= "text";
 		public $size= 0;
-		public $default= "";
+		public $default= null;
 		public $required= false;
 		public $refTo= false;
 		public $refBy= Array();
 		public $key= false;
 
+		/**
+		 * Checks if the string sent indicates that it is about a property
+		 * @param String $definition
+		 * @return boolean
+		 */
 		public static function isProperty($definition)
 		{
-			return strpos($definition, ":");
+			return strpos($definition, ":")? true: false;
 		}
 
+		/**
+		 * Returns if the passed property type is known by the system.
+		 * If it is not known, then it stores it as a doubt
+		 *
+		 * @param String $type
+		 * @return boolean
+		 */
 		public static function isKnown($type)
 		{
 			foreach(Tokenizer::$dataTypes as $k=>$validTypes)
@@ -29,18 +41,40 @@
 			return false;
 		}
 
+		/**
+		 * Verifies on the passed string, if it indicates that the current
+		 * property definition should be required or not
+		 *
+		 * @param String $expression
+		 * @return boolean
+		 */
 		public static function isRequired($expression)
 		{
 			$rx= "/".implode('|', Tokenizer::$qualifiers['notnull'])."/i";
 			return (preg_match($rx, $expression))? true: false;
 		}
 
+		/**
+		 * Returns true in case the passed expression represent a property
+		 * definition, which has terms saying it should(forcedly) treated as
+		 * a key
+		 *
+		 * @param String $expression
+		 * @return boolean
+		 */
 		public static function isKey($expression)
 		{
 			$rx= "/".implode('|', Tokenizer::$qualifiers['key'])."/i";
 			return (preg_match($rx, $expression))? true: false;
 		}
 
+		/**
+		 * This method parses the current property definition due to
+		 * identify its specification, like size, type or if it is
+		 * not null
+		 *
+		 * @return boolean
+		 */
 		private function parse()
 		{
 			$str= $this->definition;
@@ -52,12 +86,14 @@
 			$typeEnd= $typeEnd? $typeEnd- $typeStart: strLen($str);
 			$type= substr($str, $typeStart, $typeEnd);
 			$this->type= $type;
-			if(!self::isKnown($this->type))
+			$tmpType= self::isKnown($this->type);
+			if(!$tmpType)
 			{
 				//TODO: Darwin::add($this->type);
 				echo "UNKNOWN TYPE...for a while";
 				return false;
-			}
+			}else
+				$type= $tmpType;
 
 			// identifying the name
 			$this->name= substr($str, 0, $typeStart);
@@ -89,9 +125,21 @@
 				// identifying if it is a forced key
 				if(self::isKey($details))
 					$this->key= true;
+
+				// identifying its size
+				if(preg_match(PROP_SIZE, $details, $size))
+					$this->size= $size[0];
 			}
+			return true;
 		}
-		
+
+		/**
+		 * The constructor itself
+		 * IT receives by paramether, the definition string, to parse the
+		 * property's characteristics and store it on itself
+		 *
+		 * @param String $definition
+		 */
 		public function MindProperty($definition)
 		{
 			$this->definition= $definition;
--- mind3rd/API/utils/constants.php
+++ mind3rd/API/utils/constants.php
@@ -9,8 +9,9 @@
 	define('PROP_DETAILS', "/\(.*/");
 	define('PROP_DEFAULT', "/\".*\"/");
 	define('PROP_DEFEXEC', "/(^(\"=)|(\"exec\:))|(\"$)/i");
+	define('PROP_SIZE', "/\d+(\.?\d+)/");
 	define('COMA_SEPARATOR', '/\s/');
 	define('SINGLE_COMMENT', '/\/\/.+\n/');
 	define('MULTILINE_COMMENT', '/\/\*.+?\*\//');
 	define('NEW_LINE', "/\n/");
-	define('EXEC_STRING', 'exec:'); // equal (=) is also acceptable
+	define('EXEC_STRING', 'exec:'); // equal(=) is also acceptable
