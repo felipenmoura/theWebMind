be051170d61b4e1dccfed4894b5e97db0b43dbd3
Felipe Nascimento de Moura <felipenmoura@gmail.com>
2011-04-14 18:31:20 -0300

    added images to setup

--- setup.php
+++ setup.php
@@ -12,18 +12,18 @@
         exit;
     }
     
-    echo "<p><img src='ide/images/".((Setup::$phpVsOk)? 'o':'f').".png' /> PHP 5.3+<br/>";
-    echo "<p><img src='ide/images/".((Setup::$sqliteOk)? 'o':'f').".png' /> SQLite3 support<br/>";
-    echo "<p><img src='ide/images/".((Setup::$readLlineOk)? 'o':'w').".png' /> ReadLine";
+    echo "<p><img src='docs/ide/images/".((Setup::$phpVsOk)? 'o':'f').".png' /> PHP 5.3+<br/>";
+    echo "<p><img src='docs/ide/images/".((Setup::$sqliteOk)? 'o':'f').".png' /> SQLite3 support<br/>";
+    echo "<p><img src='docs/ide/images/".((Setup::$readLlineOk)? 'o':'w').".png' /> ReadLine";
     if(!Setup::$readLlineOk)
     {
         $tip=  "If you will not use the application trhough command line, but only via HTTP<br/>";
         $tip.= "this lib is not required. Once the application is installed you can access it using the HTTP protocol";
         echo "<div style='background-color:yellow;border:solid 1px brown'>$tip</div>";
     }
-    echo "<p><img src='ide/images/".((Setup::$projectsDir)? 'o':'w').".png' /> Write permissions in '".getcwd()."/mind3rd/projects/' to the user '".trim(shell_exec('whoami'))."'<br/>";
-    echo "<p><img src='ide/images/".((Setup::$sqliteDir)? 'o':'f').".png' /> Write permissions in '".getcwd()."/mind3rd/SQLite/' to the user '".trim(shell_exec('whoami'))."'<br/>";
-    echo "<p><img src='ide/images/".((Setup::$apiDir)? 'o':'f').".png' /> Write permissions in '".getcwd()."/mind3rd/API/' to the user '".trim(shell_exec('whoami'))."'<br/>";
+    echo "<p><img src='docs/ide/images/".((Setup::$projectsDir)? 'o':'w').".png' /> Write permissions in '".getcwd()."/mind3rd/projects/' to the user '".trim(shell_exec('whoami'))."'<br/>";
+    echo "<p><img src='docs/ide/images/".((Setup::$sqliteDir)? 'o':'f').".png' /> Write permissions in '".getcwd()."/mind3rd/SQLite/' to the user '".trim(shell_exec('whoami'))."'<br/>";
+    echo "<p><img src='docs/ide/images/".((Setup::$apiDir)? 'o':'f').".png' /> Write permissions in '".getcwd()."/mind3rd/API/' to the user '".trim(shell_exec('whoami'))."'<br/>";
     
     
     
